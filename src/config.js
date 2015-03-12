0fbf27807dced8a3218735ea1bab76467f5953f8
mohayonao <mohayonao@gmail.com>
2015-03-12 19:43:43 +0900

    add sc/config

---
+++ src/config.js
@@ -0,0 +1,61 @@
+"use strict";
+
+let stack = [];
+
+function assign(src, dst) {
+  Object.keys(src).forEach((key) => {
+    dst[key] = src[key];
+  });
+}
+
+function clone(src) {
+  let dst = {};
+  assign(src, dst);
+  return dst;
+}
+
+/// # Config
+/// ## sc.config
+/// - `baseFreq` _(number)_: The default value is `440`.
+/// - `sampleRate` _(number)_: The default value is `44100`.
+/// - `randomAPI` _(function)_: The default value is `Math.random`.
+///
+/// ## sc.config.save()
+/// Saves the entire state of the `sc.config` by pushing the current state onto a stack.
+///
+/// ## sc.config.restore()
+/// Restores the most recently saved `sc.config` state by popping the top entry in the state stack. If there is no saved state, this method does nothing.
+///
+/// ```js
+/// sc.config.baseFreq; // => 440
+/// sc.midicps(81);     // => 880
+///
+/// sc.config.save();
+///
+/// sc.config.baseFreq = 442
+/// sc.midicps(81); // => 884
+///
+/// sc.config.restore();
+///
+/// sc.config.baseFreq; // => 440
+/// sc.midicps(81);     // => 880
+/// ```
+export class SCConfig {
+  constructor() {
+    this.baseFreq = 440;
+    this.sampleRate = 44100;
+    this.randomAPI = Math.random;
+  }
+
+  save() {
+    stack.push(clone(this));
+  }
+
+  restore() {
+    if (stack.length) {
+      assign(stack.pop(), this);
+    }
+  }
+}
+
+export default new SCConfig();
---
+++ test/config.js
@@ -0,0 +1,37 @@
+"use strict";
+
+import assert from "power-assert";
+import config from "../src/config";
+
+describe("config", () => {
+  describe("save() / restore()", () => {
+    let rand1 = Math.random.bind(Math);
+    let rand2 = Math.random.bind(Math);
+
+    it("works", () => {
+      config.sampleRate = 44100;
+      config.randomAPI = rand1;
+
+      config.save();
+
+      assert(config.sampleRate === 44100, "saved");
+      assert(config.randomAPI === rand1, "saved");
+
+      config.sampleRate = 22050;
+      config.randomAPI = rand2;
+
+      assert(config.sampleRate === 22050);
+      assert(config.randomAPI === rand2);
+
+      config.restore();
+
+      assert(config.sampleRate === 44100, "restored");
+      assert(config.randomAPI === rand1, "restored");
+
+      config.restore();
+
+      assert(config.sampleRate === 44100, "restored 2");
+      assert(config.randomAPI === rand1, "restored 2");
+    });
+  });
+});
